44a070a124cb4ad8bf6c5ae941ed8cf2d2344ab6
holydynamited <zggshsgsgsh@gmail.com>
2025-07-19 23:21:39 +0300

    add saving cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+if (!cart) {
+  cart =  [{
   productId:
     "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity:
@@ -10,6 +14,16 @@ export let cart = [{
   quantity:1
   
   }];
+}
+
+
+ 
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+
 
 
 export function addToCart(productId) {
@@ -28,7 +42,8 @@ export function addToCart(productId) {
         productId: productId,
         quantity:1
       })
-      }
+  }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -41,4 +56,5 @@ export function removeFromCart(productId) {
   })
 
   cart = newCart;
+  saveToStorage();
 }
