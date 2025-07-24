92b9464bd650c48f6430fbae09b6ebe552422194
holydynamited <zggshsgsgsh@gmail.com>
2025-07-24 23:56:28 +0300

    add money tests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,7 +1,7 @@
 
 
 export function formatCurrency(priceCents) {
-  return (Math.round(priceCents / 100)).toFixed(2);
+  const dollars = Math.round(priceCents) / 100;
+  return dollars.toFixed(2);
 }
-
 export default formatCurrency;
